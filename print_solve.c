beacb6862cea64fb0bf4b545bfe7a38ee250fc59
Borl1seTrunov <borl1se021@icloud.com>
2023-10-18 21:31:41 +0300

    Lab2

---
+++ print_solve.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "print_solve.h"
+#include "index_first_negative.h"
+#include "index_last_negative.h"
+#include "multi_between_negative.h"
+#include "multi_before_and_after_negative.h"
+
+void print_solve(int arr[], int operation, int arr_count_items){
+	switch(operation){
+		case 0:
+			printf("%d\n", index_first_negative(arr, arr_count_items));
+			break;
+		case 1:
+			printf("%d\n", index_last_negative(arr, arr_count_items));
+			break;
+		case 2:
+			printf("%d\n", multi_between_negative(arr, arr_count_items));
+			break;
+		case 3:
+			printf("%d\n", multi_before_and_after_negative(arr, arr_count_items));
+			break;
+		default:
+			printf("Данные некорректны\n");
+			break;
+	}
+}
